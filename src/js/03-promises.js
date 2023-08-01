a6e06b77e8301b5c6888e6349e8bd14a5a33f65a
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-08-01 13:44:46 +0200

    task 03 - finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const obj = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+};
+
+obj.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(obj.inputDelay.value);
+  const step = Number(obj.inputStep.value);
+  const amount = Number(obj.inputAmount.value);
+
+  if (delay < 0 || step < 0 || amount <= 0) {
+    Notify.warning(
+      `❗ The 'first delay' and the 'delay step' must be a positive number, and the 'amount' must be greater than 0`
+    );
   } else {
-    // Reject
+    for (let i = 0; i < amount; i++) {
+      createPromise(i+1, delay)
+        .then(({ position, delay }) => {
+          Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+        delay += step;
+    }
   }
+
+  e.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
